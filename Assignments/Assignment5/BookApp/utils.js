f2b98cc1721c537e5ee480ed459cfb89b222d118
SanketChidrewar <sanketchidrewar11@gmail.com>
2020-09-09 07:38:17 +0530

    utility code for sending response to client everytime it make request for something

---
+++ Assignments/Assignment5/BookApp/utils.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+function createResult(error, data){
+    const result = {status:""}
+    if(error){
+        result.status = "error"
+        result.error = error
+    }else{
+        result.status= "success"
+        result.data = data
+    }
+     return result;
+}
+
+module.exports = {
+    createResult: createResult
+}
+
+
+
+
+
+
+
