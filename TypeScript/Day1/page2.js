791b8716d13e9e7b8d4419516925b2af1567543a
Chidrewar Sanket <sanketchidrewar11@gmail.com>
2020-09-16 09:07:01 +0530

    implicit data types in typescript

---
+++ TypeScript/Day1/page2.js
@@ -0,0 +1,30 @@
+// number
+var num = 100;
+console.log("num = " + num + ", type = " + typeof (num));
+// number
+var salary = 10.56;
+console.log("salary = " + salary + ", type = " + typeof (salary));
+// string
+var firstName = 'steve';
+console.log("firstName = " + firstName + ", type = " + typeof (firstName));
+// string
+var lastName = "Jobs";
+console.log("lastName = " + lastName + ", type = " + typeof (lastName));
+// string
+var address = "\nhome address,\ncity, \nstate,\ncountry 334224\n";
+console.log("address = " + address + ", type = " + typeof (address));
+// boolean
+var canVote = true;
+console.log("canVote = " + canVote + ", type = " + typeof (canVote));
+// undefined
+var myvar;
+console.log("myvar = " + myvar + ", type = " + typeof (myvar));
+// object
+var person = { name: "person1", address: "pune", age: 40, email: "person1@test.com" };
+console.log("person = " + person + ", type = " + typeof (person));
+// num = "test"
+// console.log(`num = ${num}, type = ${typeof(num)}`)
+// num = true
+// console.log(`num = ${num}, type = ${typeof(num)}`)
+// num = {name: "test", age: 40}
+// console.log(`num = ${num}, type = ${typeof(num)}`)
---
+++ TypeScript/Day1/page2.ts
@@ -0,0 +1,45 @@
+// number
+let num = 100
+console.log(`num = ${num}, type = ${typeof(num)}`)
+
+// number
+const salary = 10.56
+console.log(`salary = ${salary}, type = ${typeof(salary)}`)
+
+// string
+const firstName = 'steve'
+console.log(`firstName = ${firstName}, type = ${typeof(firstName)}`)
+
+// string
+const lastName = "Jobs"
+console.log(`lastName = ${lastName}, type = ${typeof(lastName)}`)
+
+// string
+const address = `
+home address,
+city, 
+state,
+country 334224
+`
+console.log(`address = ${address}, type = ${typeof(address)}`)
+
+// boolean
+const canVote = true
+console.log(`canVote = ${canVote}, type = ${typeof(canVote)}`)
+
+// undefined
+let myvar
+console.log(`myvar = ${myvar}, type = ${typeof(myvar)}`)
+
+// object
+const person = {name: "person1", address: "pune", age: 40, email: "person1@test.com"}
+console.log(`person = ${person}, type = ${typeof(person)}`)
+
+// num = "test"
+// console.log(`num = ${num}, type = ${typeof(num)}`)
+
+// num = true
+// console.log(`num = ${num}, type = ${typeof(num)}`)
+
+// num = {name: "test", age: 40}
+// console.log(`num = ${num}, type = ${typeof(num)}`)
