18bf67bdcf657c2927e6c2b320c30c5bf9db4e5d
Chidrewar Sanket <sanketchidrewar11@gmail.com>
2020-09-16 09:07:20 +0530

    data types in javascript

---
+++ TypeScript/Day1/page3.js
@@ -0,0 +1,19 @@
+// number
+let num = 100
+console.log(`num = ${num}, type = ${typeof(num)}`)
+
+num = "test"
+console.log(`num = ${num}, type = ${typeof(num)}`)
+
+num = true
+console.log(`num = ${num}, type = ${typeof(num)}`)
+
+num = {name: "test", age: 40}
+console.log(`num = ${num}, type = ${typeof(num)}`)
+
+num = 300
+console.log(`num = ${num}, type = ${typeof(num)}`)
+
+
+
+
